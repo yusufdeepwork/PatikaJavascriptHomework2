fa5a5f99a60b2ae795320085e48a2bf50a76d0c3
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-05-12 18:58:58 +0300

    add removeElement function & some styles

--- js/todoApp.js
+++ js/todoApp.js
@@ -1,7 +1,19 @@
 
 function newElement() {
+    const exit = document.createElement("span");
+    exit.className = "close"
+    exit.innerText="x"
+    exit.onclick= () => exit.parentElement.remove();
     const li = document.createElement("li")
     li.innerText=document.getElementById("task").value
     const taskList = document.getElementById("list")
+
+    li.className = li.className+" list-group-item"
     taskList.appendChild(li);
+    document.querySelectorAll("li").forEach(item => item.appendChild(exit));
+
+}
+function removeElement(exit) {
+    console.log(exit)
+      exit.parentElement.remove()
 }
